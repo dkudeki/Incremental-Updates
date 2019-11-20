703bc4f529c0d60279d166bc07e8e248b851377e
Deren Kudeki <dkudeki@illinois.edu>
2019-11-20 12:07:35 -0600

    Actually make the label a substring

--- gatherChangedRecords.py
+++ gatherChangedRecords.py
@@ -4,7 +4,7 @@ def gatherChangedRecords(changes_folder,marcjson_folder):
 	changes = {}
 	for root, dirs, files in os.walk(changes_folder):
 		for f in files:
-			label = f.find('_')+1:-4
+			label = f[f.find('_')+1:-4]
 			changes[label] = []
 			with open(root + f,'r') as infile:
 				reader = csv.reader(infile)
