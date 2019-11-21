841e41683af6b7809a42bb3edbee0ecc277eafe1
Deren Kudeki <dkudeki@illinois.edu>
2019-11-21 11:06:23 -0600

    Only read in CSV files

--- gatherChangedRecords.py
+++ gatherChangedRecords.py
@@ -69,12 +69,13 @@ def gatherChangedRecords(changes_folder,marcjson_folder,core_count):
 	changes = {}
 	for root, dirs, files in os.walk(changes_folder):
 		for f in files:
-			label = f[f.find('_')+1:-4]
-			changes[label] = []
-			with open(root + '/' + f,'r') as infile:
-				reader = csv.reader(infile)
-				for row in reader:
-					changes[label].append(row[0])
+			if f[-4:] == '.csv':
+				label = f[f.find('_')+1:-4]
+				changes[label] = []
+				with open(root + '/' + f,'r') as infile:
+					reader = csv.reader(infile)
+					for row in reader:
+						changes[label].append(row[0])
 
 	outfiles = {}
 	for group in changes:
